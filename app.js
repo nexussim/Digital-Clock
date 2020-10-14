7575b7b58f9d48cb3b83093f5e7e4348dec26719
Andrew Cave <nexus_sim2@hotmail.com>
2020-10-13 21:36:14 -0700

    Fixed typos. Slightly refactored.

---
+++ app.js
@@ -0,0 +1,56 @@
+// Border with buttons
+// containing section
+// faceplate
+// digital text
+// glowing text
+// live time
+
+var time = document.getElementById('time');
+var p1 = document.createElement('p');
+var p2 = document.createElement('p');
+var p3 = document.createElement('p');
+var p4 = document.createElement('p');
+var p5 = document.createElement('p');
+time.appendChild(p1);
+time.appendChild(p2);
+time.appendChild(p3);
+time.appendChild(p4);
+time.appendChild(p5);
+p4.textContent = ':';
+p2.textContent = ':';
+
+
+setInterval(function displayTime() {
+    
+    let currentTime = new Date();
+    let addZero = '0';
+    let getHours = currentTime.getHours();
+    let getMinutes = currentTime.getMinutes();
+    let getSeconds = currentTime.getSeconds();
+    let hours = getHours.toString();
+    let minutes = getMinutes.toString();
+    let seconds = getSeconds.toString();
+
+    
+    if (hours.length === 1)  {
+        var twoIntegerHour = addZero.concat(hours);
+        p1.textContent = twoIntegerHour;
+    } else {
+        p1.textContent = hours;
+    }
+    
+    if (minutes.length === 1)  {
+        var twoIntegerMinute = addZero.concat(minutes);
+        p3.textContent = twoIntegerMinute;
+    } else {
+        p3.textContent = minutes;
+    }
+
+    if (seconds.length === 1)  {
+        var twoIntegerSeconds = addZero.concat(seconds);
+        p5.textContent = twoIntegerSeconds;
+    } else {
+        p5.textContent = seconds;
+    }
+
+}, 1000)
