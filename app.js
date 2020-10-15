c311ce4480c236568daa18ee797c7cf3cf8fa576
Andrew Cave <nexus_sim2@hotmail.com>
2020-10-14 18:12:58 -0700

    Refactored some repetition.

--- app.js
+++ app.js
@@ -1,72 +1,54 @@
-// Border with buttons
-// containing section
-// faceplate
-// digital text
-// glowing text
-// live time
-
 var time = document.getElementById('time');
 var amPm = document.getElementById('AmPmPosition');
-var p1 = document.createElement('p');
-var p2 = document.createElement('p');
-var p3 = document.createElement('p');
-var p4 = document.createElement('p');
-var p5 = document.createElement('p');
-var p6 = document.createElement('p');
-var p7 = document.createElement('p');
-time.appendChild(p1);
-time.appendChild(p2);
-time.appendChild(p3);
-time.appendChild(p4);
-time.appendChild(p5);
-time.appendChild(p6);
-time.appendChild(p7);
-
-
-
+var div1 = document.createElement('div');
+var div2 = document.createElement('div');
+var div3 = document.createElement('div');
+var div4 = document.createElement('div');
+var div5 = document.createElement('div');
+var div6 = document.createElement('div');
+time.appendChild(div1);
+time.appendChild(div2);
+time.appendChild(div3);
+time.appendChild(div4);
+time.appendChild(div5);
+time.appendChild(div6);
 
 
 setInterval(function displayTime() {
     
     let currentTime = new Date();
-    let addZero = '0';
     let getHours = currentTime.getHours();
-    let getMinutes = currentTime.getMinutes();
-    let getSeconds = currentTime.getSeconds();
+    getHours = hours12(currentTime);
     let hours = getHours.toString();
+    let getMinutes = currentTime.getMinutes();
     let minutes = getMinutes.toString();
+    let getSeconds = currentTime.getSeconds();
     let seconds = getSeconds.toString();
 
+    addDigit(hours, div1)
+    div2.textContent = ':';
+    addDigit(minutes, div3)
+    div4.textContent = ':';
+    addDigit(seconds, div5)
     
-    if (hours.length === 1)  {
-        var twoIntegerHour = addZero.concat(hours);
-        p1.textContent = twoIntegerHour;
-    } else {
-        p1.textContent = hours;
-    }
-
-    p2.textContent = ':';
-    
-    if (minutes.length === 1)  {
-        var twoIntegerMinute = addZero.concat(minutes);
-        p3.textContent = twoIntegerMinute;
-    } else {
-        p3.textContent = minutes;
-    }
-
-    p4.textContent = ':';
-
-    if (seconds.length === 1)  {
-        var twoIntegerSeconds = addZero.concat(seconds);
-        p5.textContent = twoIntegerSeconds;
-    } else {
-        p5.textContent = seconds;
-    }
-
     if (hours < 12) {
-        p6.textContent = 'am';
+        amPm.textContent = 'am';
     } else {
-        p6.textContent = 'pm';
+        amPm.textContent = 'pm';
     }
 
 }, 250)
+
+function addDigit(time, location) {
+    var addZero = '0';
+    if (time.length === 1)  {
+        var twoIntegerHour = addZero.concat(time);
+        location.textContent = twoIntegerHour;
+    } else {
+        location.textContent = time;
+    }
+}
+
+function hours12(date) { 
+    return (date.getHours() + 24) % 12 || 12; 
+}
